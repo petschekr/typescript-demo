4da38510222affda1661bd8d7a7f84e5a78e1765
Ryan Petschek <petschekr@gmail.com>
2018-02-11 13:59:03 -0500

    Implement settings

--- settings.ts
+++ settings.ts
@@ -2,5 +2,20 @@
 let settingStore: {
 	[settingName: string]: any
 } = {};
-// getSetting(name: string, value: any): T
-// updateSetting()
+
+export function getSetting<T>(name: string, value?: T): T {
+	if (settingStore[name]) {
+		return settingStore[name] as T;
+	}
+	else if (value === undefined) {
+		throw new Error("Setting not found!");
+	}
+	else {
+		updateSetting(name, value);
+		return value;
+	}
+}
+
+export function updateSetting<T>(name: string, value: T): void {
+	settingStore[name] = value;
+}
